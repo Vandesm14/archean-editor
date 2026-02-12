02618c3cf4f9f55ae400444505c48519742f7a01
Leon Davis <leonskij@proton.me>
2026-02-12 23:42:31 +0000

    Enable shadows on directional light.

--- src/main.rs
+++ src/main.rs
@@ -84,6 +84,7 @@ fn setup(
     .with_child((
       DirectionalLight {
         illuminance: 5000.0,
+        shadows_enabled: true,
         ..Default::default()
       },
       Transform::from_xyz(10.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
