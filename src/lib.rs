83b7c8fd847adac1d6403725cc4d75c06e446c97
Leon Davis <leonskij@proton.me>
2026-02-16 23:14:26 +0000

    Switch to mesh rotations instead of unique models per block type.

--- src/lib.rs
+++ src/lib.rs
@@ -57,85 +57,36 @@ pub struct Selected;
 
 #[derive(Resource)]
 pub struct CommonAssets {
-  blocks: [Handle<Mesh>; 53],
+  pub cube: Handle<Mesh>,
+  pub slope: Handle<Mesh>,
+  pub corner: Handle<Mesh>,
+  pub pyramid: Handle<Mesh>,
+  pub inv_corner: Handle<Mesh>,
   pub unselected: Handle<StandardMaterial>,
   pub selected: Handle<StandardMaterial>,
-}
-
-impl CommonAssets {
-  pub fn block(&self, id: u8) -> Handle<Mesh> {
-    self
-      .blocks
-      .get(id as usize)
-      .cloned()
-      .unwrap_or_else(|| self.blocks[0].clone())
-  }
 }
 
 impl FromWorld for CommonAssets {
   fn from_world(world: &mut World) -> Self {
     let mut materials = world.resource_mut::<Assets<StandardMaterial>>();
+
     let unselected = materials.add(Color::WHITE);
     let selected = materials.add(Color::from(css::BLUE));
 
     let asset_server = world.resource::<AssetServer>();
 
+    let cube = asset_server.load("blocks/cube.obj");
+    let slope = asset_server.load("blocks/slope.obj");
+    let corner = asset_server.load("blocks/corner.obj");
+    let pyramid = asset_server.load("blocks/pyramid.obj");
+    let inv_corner = asset_server.load("blocks/inv_corner.obj");
+
     Self {
-      blocks: [
-        asset_server.load("blocks/00.obj"),
-        asset_server.load("blocks/01.obj"),
-        asset_server.load("blocks/02.obj"),
-        asset_server.load("blocks/03.obj"),
-        asset_server.load("blocks/04.obj"),
-        asset_server.load("blocks/05.obj"),
-        asset_server.load("blocks/06.obj"),
-        asset_server.load("blocks/07.obj"),
-        asset_server.load("blocks/08.obj"),
-        asset_server.load("blocks/09.obj"),
-        asset_server.load("blocks/10.obj"),
-        asset_server.load("blocks/11.obj"),
-        asset_server.load("blocks/12.obj"),
-        asset_server.load("blocks/13.obj"),
-        asset_server.load("blocks/14.obj"),
-        asset_server.load("blocks/15.obj"),
-        asset_server.load("blocks/16.obj"),
-        asset_server.load("blocks/17.obj"),
-        asset_server.load("blocks/18.obj"),
-        asset_server.load("blocks/19.obj"),
-        asset_server.load("blocks/20.obj"),
-        asset_server.load("blocks/21.obj"),
-        asset_server.load("blocks/22.obj"),
-        asset_server.load("blocks/23.obj"),
-        asset_server.load("blocks/24.obj"),
-        asset_server.load("blocks/25.obj"),
-        asset_server.load("blocks/26.obj"),
-        asset_server.load("blocks/27.obj"),
-        asset_server.load("blocks/28.obj"),
-        asset_server.load("blocks/29.obj"),
-        asset_server.load("blocks/30.obj"),
-        asset_server.load("blocks/31.obj"),
-        asset_server.load("blocks/32.obj"),
-        asset_server.load("blocks/33.obj"),
-        asset_server.load("blocks/34.obj"),
-        asset_server.load("blocks/35.obj"),
-        asset_server.load("blocks/36.obj"),
-        asset_server.load("blocks/37.obj"),
-        asset_server.load("blocks/38.obj"),
-        asset_server.load("blocks/39.obj"),
-        asset_server.load("blocks/40.obj"),
-        asset_server.load("blocks/41.obj"),
-        asset_server.load("blocks/42.obj"),
-        asset_server.load("blocks/43.obj"),
-        asset_server.load("blocks/44.obj"),
-        asset_server.load("blocks/45.obj"),
-        asset_server.load("blocks/46.obj"),
-        asset_server.load("blocks/47.obj"),
-        asset_server.load("blocks/48.obj"),
-        asset_server.load("blocks/49.obj"),
-        asset_server.load("blocks/50.obj"),
-        asset_server.load("blocks/51.obj"),
-        asset_server.load("blocks/52.obj"),
-      ],
+      cube,
+      slope,
+      corner,
+      pyramid,
+      inv_corner,
       unselected,
       selected,
     }
